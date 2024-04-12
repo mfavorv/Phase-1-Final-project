14289cec88809f5f31a127564d91fbb8e5790175
Favor Vincent <vfavorm77@gmail.com>
2024-04-12 19:02:41 +0300

    Final JS-12

--- src/index.js
+++ src/index.js
@@ -19,5 +19,21 @@ document.addEventListener("DOMContentLoaded", () => {
             .catch(error => console.error("Error:", error));
     };
     fetchCategories();
+     //START QUIZ FUNCTION
+     function startQuiz() {
+        const startQuizButton = document.getElementById("begin");
+        startQuizButton.addEventListener("click", (event) => {
+            event.preventDefault();
+            
+            results = 0;
+            const category = document.getElementById("category").value;
+            const difficulty = document.getElementById("difficulty").value;
+            const amount = document.getElementById("number").value || 10;
+            questions = document.getElementById("quiz-container");
+            questions.innerHTML = ""; 
+
+        })
+    }
+    startQuiz()
 
 })
